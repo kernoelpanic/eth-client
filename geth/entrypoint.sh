2e42a773360a57f1e2813f6b48c833bdc38857fc
aljosha <kernoelpanic@3-volution.net>
2025-12-17 10:13:46 +0100

    updated chainid for 20251201

--- geth/entrypoint.sh
+++ geth/entrypoint.sh
@@ -7,7 +7,7 @@ pwd
 
 if [ -e ./datadir/bob/initialized ] && [ ! -e ./datadir/bob/initlock ]; then
     echo "Running geth ..."
-    exec geth --verbosity 5 --networkid '20250101' --config ${WORKDIR_CONTAINER}/config.toml
+    exec geth --verbosity 5 --networkid '20251201' --config ${WORKDIR_CONTAINER}/config.toml
 else
     # This finishes fasts and terminates
     # container will be restarted by docker compose after termination
@@ -15,7 +15,7 @@ else
     touch ${WORKDIR_CONTAINER}/datadir/bob/initlock
     geth \
     --identity 'bob' \
-    --networkid '20250101' \
+    --networkid '20251201' \
     --datadir ${WORKDIR_CONTAINER}/datadir/bob \
     --verbosity 5 \
     init ${WORKDIR_CONTAINER}/genesis.json
